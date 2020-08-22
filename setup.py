bd6c5183a6bded73cd2ba5230b46676a58153d73
Jovanni Hernandez <jovanni.m.hernandez@gmail.com>
2020-08-22 08:44:35 +0000

    Packaing is hard yo!

--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2017, Project Jupyter
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 """
 Setup Module to setup Python Handlers for the jlab_ext_example extension.
 """
@@ -23,10 +53,10 @@ version = get_version(pjoin(name, "_version.py"))
 
 lab_path = pjoin(HERE, name, "labextension")
 
-## Representative files that should exist after a successful build
-#jstargets = [
-#    pjoin(HERE, "lib", "jlabextexample.js"),
-#]
+# Representative files that should exist after a successful build
+jstargets = [
+    pjoin(HERE, "lib", "plugin.js"),
+]
 
 package_data_spec = {
     name: [
@@ -37,7 +67,7 @@ package_data_spec = {
 data_files_spec = [
     ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
     ("etc/jupyter/jupyter_notebook_config.d",
-     "jupyter-config", "jupyter_scheduler.json"),
+     "jupyter-config", "jupyterlab_scheduler.json"),
 ]
 
 cmdclass = create_cmdclass("jsdeps",
@@ -45,10 +75,10 @@ cmdclass = create_cmdclass("jsdeps",
     data_files_spec=data_files_spec
 )
 
-#cmdclass["jsdeps"] = combine_commands(
-#    install_npm(HERE, build_cmd="build:all", npm=["jlpm"]),
-#    ensure_targets(jstargets),
-#)
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(HERE, build_cmd="build:all", npm=["jlpm"]),
+    ensure_targets(jstargets),
+)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -56,15 +86,15 @@ with open("README.md", "r") as fh:
 setup_args = dict(
     name=name,
     version=version,
+    url="https://github.com/tiburon-security/jupyterlab_scheduler",
     author="Jovanni Hernandez",
     description="CRON scheduler for Jupyter Lab",
     long_description= long_description,
     long_description_content_type="text/markdown",
-    #cmdclass= cmdclass,
+    cmdclass= cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
         "jupyterlab~=2.0",
-        "python-crontab~=2.5"
     ],
     zip_safe=False,
     include_package_data=True,
@@ -72,7 +102,7 @@ setup_args = dict(
     platforms="Linux, Mac OS X, Windows",
     keywords=["Jupyter", "JupyterLab"],
     classifiers=[
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
